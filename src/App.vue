fc09ea8280c641934e994de326d0e2f5c6ff78ad
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-04-05 12:40:51 +0300

    start

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,39 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div :id="$style.app">
+    <h1>{{ title }}</h1>
+    <ButtonAll @:click="increment" type="button" outlined>click me</ButtonAll>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import ButtonAll from "./components/Button";
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+  name: "App",
+  components: { ButtonAll },
+  data() {
+    return { amoutOfClicks: 0 };
+  },
+  computed: {
+    title() {
+      return `Amout of clicks ${this.amoutOfClicks}`;
+    },
+  },
+  methods: {
+    increment() {
+      this.amoutOfClicks += 1;
+    },
+  },
+};
 </script>
 
-<style>
+<style module>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 30px;
 }
 </style>
