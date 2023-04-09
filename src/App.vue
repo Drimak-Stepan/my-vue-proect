cc7b8c4e947d402d2da221f6f77e3bd8895c736e
Stepan Drimak <108022018+Drimak-Stepan@users.noreply.github.com>
2023-04-09 21:14:51 +0300

    finish 19 lesson

--- src/App.vue
+++ src/App.vue
@@ -1,42 +1,36 @@
 <template>
-  <div :id="$style.app">
-    <CustomSelect :items="[name, label]" />
-    <ApartmentsList :items="apartments">
-      <!-- <template v-slot:title>Title</template> -->
-      <template v-slot:apartment="{ apartment }">
-        <ApartmentsItem
-          :key="apartment.id"
-          :descr="apartment.descr"
-          :rating="apartment.rating"
-          :imgSrc="apartment.imgUrl"
-          :price="apartment.p" /></template
-    ></ApartmentsList>
+  <div id="app">
+    <div class="content">
+      <AppHeader />
+      <router-view></router-view>
+    </div>
+    <AppFooter />
   </div>
 </template>
 
 <script>
-import ApartmentsList from "./components/apartment/ApartmentsList";
-import ApartmentsItem from "./components/apartment/ApartmentsItem";
-import CustomSelect from "./components/shared/CustomSelect";
-import apartments from "./components/apartment/apartaments";
+import AppFooter from "./components/Footer";
+import AppHeader from "./components/Header";
 export default {
   name: "App",
-  components: { ApartmentsList, ApartmentsItem, CustomSelect },
-  data() {
-    return {
-      apartments,
-    };
+  components: {
+    AppFooter,
+    AppHeader,
   },
 };
 </script>
 
 <style module>
 #app {
+  display: flex;
+  flex-direction: column;
+  min-height: 100vh;
   font-family: Montserrat, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 30px;
+}
+.content {
+  flex-grow: 1;
+  padding-top: 120px;
 }
 </style>
